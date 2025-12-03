b7d498d10b0a71fa5ed34641bf2c94d3ac316618
Bhuvangs04 <bhuvangs2004@gmail.com>
2025-12-04 01:26:13 +0530

    updated db config

--- auth_service/config/db.js
+++ auth_service/config/db.js
@@ -2,7 +2,15 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, {
+            maxPoolSize: 50,                // handle concurrency
+            minPoolSize: 10,                // keep warm connections
+            serverSelectionTimeoutMS: 3000, // fail fast if DB unreachable
+            socketTimeoutMS: 45000,         // avoid random socket disconnects
+            maxIdleTimeMS: 30000,
+            family: 4,                      // force IPv4 (fixes DNS delay sometimes)
+            retryWrites: true
+        });
         console.log("Auth Service: MongoDB Connected ✔");
     } catch (error) {
         console.error("MongoDB Error", error.message);
