7f324ac8ace1396529510551574810b90a05109a
unkown-prog <bhuvi4662@gmail.com>
2026-01-02 21:22:25 +0530

    added

--- auth_service/models/user.model.js
+++ auth_service/models/user.model.js
@@ -27,7 +27,7 @@ const userSchema = new mongoose.Schema(
         disabledUntil: Date,
         role: {
             type: String,
-            enum: ["super_admin", "college_admin", "teacher", "student", "parent", "HOD", "HOD_ASSISTANT", "STAFF"],
+            enum: ["super_admin", "college_admin", "teacher", "student", "parent", "HOD", "HOD_ASSISTANT", "FACULTY"],
             required: true
         },
         collegeId: {
